9bf8f53ec896440770b4cba7231a58de3e49a894
jaime3e <jaime3e@gmail.com>
2016-07-21 23:21:07 -0700

    more config

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,34 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify= require('gulp-uglify');
+var plumber= require('gulp-plumber');
 
 
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', ['sass','scripts','sass:watch','scripts:watch']);
+
+
+gulp.task('scripts',function(){
+	gulp.src('./src/js/*.js')
+	.pipe(plumber())
+	.pipe(uglify())
+	.pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('sass', function () {
 	process.stdout.write(''+ '\n');
   return gulp.src('./src/sass/main.scss')
+  	.pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./build/css'));
 });
  
 gulp.task('sass:watch', function () {
   gulp.watch('./src/sass/main.scss', ['sass']);
+});
+
+gulp.task('scripts:watch', function () {
+  gulp.watch('./src/js/*.js', ['scripts']);
 });
 
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+'use strict'
+//this is a comment
+var a=10;
+var name='garcia';
